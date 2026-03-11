a96a5b9ba3eefe59d7bc45b074fa20573d98bb19
Arumuga-Raja-p <arumugarajap007@gmail.com>
2026-03-11 23:36:42 +0530

    Revert "chore: update dependencies and refactor schema types"
    This reverts commit e8ebe88943b3e9ad2088274d94a0e2729da7b25d.

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,6 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
 
-import property from './property'
-import testimonal from './testimonal'
-
-
-export const schemaTypes = [testimonal, property]
+export const schemaTypes = [post, author, category, blockContent]
